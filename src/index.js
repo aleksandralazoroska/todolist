9dc186dadffa66743ce9580b074aef83c6031a85
Aleksandra Lazoroska <aleksandra.lazoroska@gmail.com>
2019-06-05 12:18:49 +0200

    Adding ordered  list

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById('root'));
+ReactDOM.render(
+  <ol>
+    <li>The capital of Spain is Madrid</li>
+    <li>The capital of France is Paris</li>
+    <li>The capital of Portugal is Lisabon</li>
+    <li>The capital of Sweden is Stockholm</li>
+    <li>The capital of England is London</li>
+  </ol>,
+  document.getElementById('root')
+);

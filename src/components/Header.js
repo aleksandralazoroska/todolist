81aa3ed96e2e844b071e685e44e664deab05c2f7
Aleksandra Lazoroska <aleksandra.lazoroska@gmail.com>
2019-06-10 23:43:55 +0200

    Creating child components

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <nav>
+            <h2>This is Header</h2>
+        </nav>
+    )
+}
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-function MyInfo(){
-  return (
-    <div class="container-biography">
-      <h1>Aleksandra</h1>
-      <p>I'm from Macedonia, and work how Frontend Developer</p>
-      <ul>
-        <li>Santorini</li>
-        <li>Malta</li>
-        <li>Turkey</li>
-      </ul>
-    </div>
-
-  )
-}
+import App from "./App.js"
+import './style.css';
 
 ReactDOM.render (
-  <MyInfo />,
+  <App />,
   document.getElementById('root')
 )

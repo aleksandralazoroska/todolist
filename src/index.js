016a38c251abbf31ff9f5ffb88e0eeaa632731eb
Aleksandra Lazoroska <aleksandra.lazoroska@gmail.com>
2019-06-12 14:27:33 +0200

    Creating sample-condition

--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from "./App.js"
 import './style.css';
 
-ReactDOM.render (
+function App() {
+  const date = new Date()
+  const hours = date.getHours()
+  let timeOftheDay
+  const styles= {
+    fontSize: 25 
+  }
+
+  if (hours < 10 ) {
+    timeOftheDay = "morning"
+    styles.color= "yellow"
+  } else if (hours >= 10 && hours < 18 ) {
+    timeOftheDay = "afternoon"
+    styles.color= "orange"
+  } else  {
+    timeOftheDay = "night"
+    styles.color= "blue"
+  }
+
+  return (
+    <h1 style={styles}>Good {timeOftheDay}!</h1>
+  )
+}
+
+ReactDOM.render(
   <App />,
   document.getElementById('root')
 )
-

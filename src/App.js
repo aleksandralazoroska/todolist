2d6c0ce56134fd1cf22c3498bd50ec62dfe776d1
Aleksandra Lazoroska <aleksandra.lazoroska@gmail.com>
2019-06-17 16:52:07 +0200

    Add exercises

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
-import Joke from './Joke.js';
+import Joke from './components/Joke.js';
 
 function App() {
     return (
